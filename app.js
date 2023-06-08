601e2b1673130d6b1b9d188cf46bb6deedbc37ce
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-06-08 14:53:07 -0600

    WIP_Launching Server

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
+
 app.set('view engine', 'ejs')
+app.listen(3000)
+
 app.use((req, res, next)=>{
     console.log('host:', req.hostname);
     console.log('path: ', req.path);
     console.log('method:', req.method);
     next()
 })
-
+app.use(morgan('tiny'))
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
